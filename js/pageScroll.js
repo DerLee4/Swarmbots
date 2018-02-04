2bbd4185e0315831a47158847f197aa2b6cac1b6
DerLee4 <derek8lee@gmail.com>
2018-02-03 18:34:18 -0800

    Add files via upload
    Uploads Include: js for page scrolling

---
+++ js/pageScroll.js
@@ -0,0 +1,21 @@
+/*CSS Modified from: https://gist.github.com/CodeMyUI/9e60eaddc810c3aac9558af52859459b */
+
+body {
+  background: #222;
+  color: #f0f0f0;
+}
+#messenger {
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  
+  white-space: nowrap;
+  text-shadow: 0 2px 2px rgba(0, 0, 0, 0.9);
+    
+    
+  font: 30px Montserrat, sans-serif;
+  line-height: 1.8;
+  color: #f5f6f7;
+}
